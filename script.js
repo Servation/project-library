4f86f7a4b1e4da0dc0f13a8e623b75b8964c8f00
Servation <gwfds123@gmail.com>
2021-12-14 21:19:05 -0800

    started functions for adding books to array and making them appear

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let myLibrary = [];
+let bookCounter = 0;
 
-function Book(title, author, read) {
+function Book(title, author, read, bookID) {
     this.title = title
     this.author = author
     this.read = read
+    this.bookID = bookID
 }
 
 Book.prototype.toggleRead = function () {
@@ -15,5 +17,32 @@ Book.prototype.toggleRead = function () {
 }
 
 function addBookToLibrary() {
+    let bookTitle = document.getElementById("title").value;
+    let bookAuthor = document.getElementById("author").value;
+    let bookRead = document.getElementById("hasRead").checked;
+    let bookObj = new Book(bookTitle, bookAuthor, bookRead, bookCounter);
+    myLibrary.push(bookObj);
+    bookCounter++;
+    showLibrary()
+}
 
+function showLibrary() {
+    let bookLib = document.getElementById("theLibrary")
+    while (bookLib.firstChild) {
+        bookLib.removeChild(bookLib.lastChild);
+    }
+    myLibrary.forEach(el => {
+        let book = document.createElement("div");
+        book.setAttribute('id', el.bookID);
+        let title = document.createElement("h2");
+        let author = document.createElement("h3");
+        let read = document.createElement("p");
+        title.textContent = "Title: " + el.title;
+        author.textContent = "Author: " + el.author;
+        read.textContent = "Read: " + el.read;
+        bookLib.appendChild(book);
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(read);
+    });
 }
