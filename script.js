5d8c0e4123d2392091d5582f6cbc9f972f9fb3b6
Servation <gwfds123@gmail.com>
2021-12-15 22:38:14 -0800

    added pages and delete button to books

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 let bookCounter = 0;
 
-function Book(title, author, read, bookID) {
+function Book(title, author, pages, read, bookID) {
     this.title = title
     this.author = author
+    this.pages = pages
     this.read = read
     this.bookID = bookID
 }
@@ -19,8 +20,9 @@ Book.prototype.toggleRead = function () {
 function addBookToLibrary() {
     let bookTitle = document.getElementById("title").value;
     let bookAuthor = document.getElementById("author").value;
+    let bookPages = document.getElementById("pages").value;
     let bookRead = document.getElementById("hasRead").checked;
-    let bookObj = new Book(bookTitle, bookAuthor, bookRead, bookCounter);
+    let bookObj = new Book(bookTitle, bookAuthor, bookPages, bookRead, bookCounter);
     myLibrary.push(bookObj);
     bookCounter++;
     showLibrary()
@@ -36,13 +38,25 @@ function showLibrary() {
         book.setAttribute('id', el.bookID);
         let title = document.createElement("h2");
         let author = document.createElement("h3");
+        let pages = document.createElement("p");
         let read = document.createElement("p");
+        let deleteButton = document.createElement("button");
         title.textContent = "Title: " + el.title;
         author.textContent = "Author: " + el.author;
+        pages.textContent = "Pages: " + el.pages;
         read.textContent = "Read: " + el.read;
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function () {
+            myLibrary = myLibrary.filter(function (obj) {
+                return obj.bookID !== el.bookID;
+            });
+            showLibrary();
+        });
         bookLib.appendChild(book);
         book.appendChild(title);
         book.appendChild(author);
+        book.appendChild(pages);
         book.appendChild(read);
+        book.appendChild(deleteButton);
     });
 }
